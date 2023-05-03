aee90a747f113cc75792b7bce55dbea9134860ce
Mary <2904lampa@gmail.com>
2023-05-03 17:48:48 +0300

    Initial commit

---
+++ src/test/java/ru/itgirl/libraryproject/LibraryProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.itgirl.libraryproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
